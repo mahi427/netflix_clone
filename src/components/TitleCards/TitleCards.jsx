a8b0efab9603b4aa971b33ab1181e679bbd9b632
Mehak Arora <Tailwind>
2025-07-18 11:34:13 +0530

    Initial commit

---
+++ src/components/TitleCards/TitleCards.jsx
@@ -0,0 +1,34 @@
+import React, { useEffect, useRef} from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data' // Assuming you have a data file for the cards
+
+
+const TitleCards = () => {
+  const cardsRef = useRef();
+
+const handleWheel = (event) => {
+  event.preventDefault();
+  cardsRef.current.scrollLeft += event.deltaY;
+}
+
+useEffect(() => {
+  cardsRef.current.addEventListener('wheel', handleWheel);
+},[])
+
+
+  return (
+    <div className='title-Cards'>
+      <h2>Popular on Netflix</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => (
+          <div className='card' key={index}>
+            <img src={card.image} alt="" />
+            <p>{card.name}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default TitleCards
